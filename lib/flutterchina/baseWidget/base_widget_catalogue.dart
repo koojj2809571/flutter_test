8e92ba49ed3f66ed383cb4aedce79d4819db6196
Opipi <koojj2809571@iCloud.com>
2019-01-06 21:48:27 +0800

    1111

---
+++ lib/flutterchina/baseWidget/base_widget_catalogue.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new BaseWidgetCatalogue());
+
+class BaseWidgetCatalogue extends StatelessWidget{
+  final String title = "基本组件目录";
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: title,
+      theme: ThemeData(primarySwatch: Colors.black45),
+      home: new CatalogueHomePage(title: title,),
+    );
+  }
+
+}
+
+class CatalogueHomePage extends StatelessWidget{
+  CatalogueHomePage({Key key, this.title}):super(key:key);
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: new Center(
+          child: Text(title),
+        ),
+      ),
+      body: new Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 10.0),
+            child: new Row(
+
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_app/baseWidget/main.dart';
+import 'package:flutter_app/flutterchina/baseWidget/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
